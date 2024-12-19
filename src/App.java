a157d0458e8b5f8959c94a1e2ee9929a1c188809
quocanhn20 <anhnqse183620@fpt.edu.vn>
2024-12-19 21:07:43 +0700

    lan 1.2 thay doi

--- src/App.java
+++ src/App.java
@@ -2,4 +2,42 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
     }
+    private int name;
+    private int age;
+    private int address;
+    public App(int name, int age, int address) {
+        this.name = name;
+        this.age = age;
+        this.address = address;
+    }
+
+    public int getName() {
+        return name;
+    }
+    
+    public int getAge() {
+        return age;
+    }
+
+    public int getAddress() {
+        return address;
+    }
+
+    public void setName(int name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setAddress(int address) {
+        this.address = address;
+    }
+
+    public void display() {
+        System.out.println("Name: " + name + " Age: " + age + " Address: " + address);
+    }
+
+    
 }
